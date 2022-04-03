846fc88da041213d0cbff816782ef4ae32e90ac0
Erwelus <AndreyLyubkin@mail.ru>
2022-04-04 00:50:19 +0300

    improved validating

--- src/main/java/com/ervelus/security/TokenProvider.java
+++ src/main/java/com/ervelus/security/TokenProvider.java
@@ -2,37 +2,40 @@ package com.ervelus.security;
 
 import com.ervelus.infrastructure.annotations.InjectProperty;
 import io.jsonwebtoken.*;
+import lombok.Setter;
 
 import java.util.Date;
 
 public class TokenProvider {
     @InjectProperty("secret")
+    @Setter
     private String tokenSecret;
 
     public String createToken(String username){
-        Date now = new Date();
-        Date expired = new Date(now.getTime() + 30*1000*60);
+        if (username==null || username.isEmpty()) return null;
         return Jwts.builder()
                 .setSubject(username)
-                .setExpiration(expired)
                 .signWith(SignatureAlgorithm.HS256, tokenSecret)
                 .compact();
     }
 
     public boolean validateToken(String token){
         try {
-            Jws<Claims> claims = Jwts.parser().setSigningKey(tokenSecret).parseClaimsJws(token);
-            return !claims.getBody().getExpiration().before(new Date());
+            Jwts.parser().setSigningKey(tokenSecret).parseClaimsJws(token);
+            return true;
         }catch (JwtException | IllegalArgumentException ex){
             return false;
         }
     }
 
     public String getUsernameFromToken(String token){
-        return Jwts.parser().setSigningKey(tokenSecret).parseClaimsJws(token).getBody().getSubject();
+        if (validateToken(token)) {
+            return Jwts.parser().setSigningKey(tokenSecret).parseClaimsJws(token).getBody().getSubject();
+        }else return null;
     }
 
     public String resolveToken(String request){
+        if (request==null) return null;
         return request.split("&")[0];
     }
 }
--- src/main/java/com/ervelus/security/TokenSecurityFilter.java
+++ src/main/java/com/ervelus/security/TokenSecurityFilter.java
@@ -2,10 +2,12 @@ package com.ervelus.security;
 
 import com.ervelus.infrastructure.annotations.Component;
 import com.ervelus.infrastructure.annotations.InjectByType;
+import lombok.Setter;
 
 @Component
 public class TokenSecurityFilter implements SecurityFilter{
     @InjectByType
+    @Setter
     private TokenProvider provider;
     @Override
     public boolean doFilter(String request) {
